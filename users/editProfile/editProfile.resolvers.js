6914651316ec8967b48a03b779f31a8bc13bec41
lunaticholic <lunaticholic@gmail.com>
2021-04-22 21:48:27 +0400

    2.16 File Upload Part Three - 서버실행 2초, 사진 정보 확인하기

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -6,10 +6,13 @@ import { protectedResolver } from "../users.utils";
 
 const resolverFn = async (_, { username, email, password: newPassword , bio, avatar}, { loggedInUser }) => {
     // console.log(loggedInUser)
-    console.log(avatar);
+    // console.log(avatar);
+    const { filename, createReadStream } = await avatar;
+    const stream = createReadStream();
     let uglyPassword = null;
+    console.log(stream);
     if (newPassword) { uglyPassword = await bcrypt.hash(newPassword, 10) }
-  const updatedUser = await client.user.update({
+    const updatedUser = await client.user.update({
         where: {
             id: loggedInUser.id,
         },
@@ -38,17 +41,26 @@ export default {
     그 점을 고려하는게 1번째 목적이고
     두번째는 password는 반드시 hashing된 password가 저장되어야 한다는 것을 기억해야 한다.
 
-    12번째 줄
+    10번째 줄
+    Avatar를 통해 불러오는 사진의 정보를 보면 filename, createReadStream이라는 정보가 내장되어 있다.
+    그 정보를 가지고 본인의 프로필 사진을 불러오면 된다.
+
+    11번째 줄
+    그래서 모든 파일을 stream을 통해서 받아낼 것이다.
+    어떤 것을? 사진에 담겨 있는 정보 중 createReadStream이라는 내부의 정보를 불러올것이다.
+    참고로 이 createReadStream만 다시 불러오면 엄청나게 많은 정보가 들어있음을 알 수 있다.
+
+    14번째 줄
     그래서 그 점을 고려해서 bcrypt를 불러와서 다시 hashing한 다음 저장해야 한다.
 
-    15번째 줄
+    21번째 줄
     uglyPassword에 값이 있다면 password에는 uglyPassword값을 할당하여 저장할 것이다.
 
-    16번째 if문
+    24번째 if문
     당연한거다. 13번째 줄이 정확하다면 ok에는 true가 담겨 Profile이 수정될 것이고
     그렇지 않다면 ok에는 false가, error에는 메세지가 출력될 것이다.
 
-    24번째 줄
+    35번째 줄
     protectedResolver를 호출하게 되면 어떤 현상이 발생하는냐?
     여기에 user가 로그인 되어 있는지 안되어 있는지 선행을 시작하게 됨. 로그인 안되어 있으면? 못하는거지 뭐
 */
@@ -56,4 +68,9 @@ export default {
 /*
     이 곳의 구문을 실행하려면 Altair를 실행해서 하면 되는데
     Altair는 Mutation을 보내기 전에 type을 확인해준다.
+*/
+
+/*
+    Avatar를 통해 저장되는 사진의 정보를 확인하면 promise로 불러와진다.
+    그 안에 우리가 사용할 수 있는 정보가 불러와진다.
 */
